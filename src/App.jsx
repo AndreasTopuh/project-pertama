5a2a190b4ede2136f916990651715d0c233f0a19
AndreasTopuh <andreasjeno23@gmail.com>
2023-10-25 11:33:28 +0800

    adding files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar"
+import Body from "./body/Body"
+
+export default function App() {
+  return (
+    <>
+    <div>
+      <Navbar />
+    </div>
+    <div>
+      <Body />
+    </div>
+    </>
+  )
+}
